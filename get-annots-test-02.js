af3d52dcabd5e9bb2dac316bc4c676e2a52b1677
timshepherd-mae <timshepherd@murphygroup.co.uk>
2026-07-07 20:07:40 +0100

    page id added to worldPnt set

--- get-annots-test-02.js
+++ get-annots-test-02.js
@@ -4,55 +4,73 @@ function testPolygonsB()
 	console.clear();
 	console.println("Running testPolygonsB...");
 
-	var annots = this.getAnnots({ nPage: this.pageNum });
+	var totalPages = this.numPages;
+	console.println("Total pages: " + totalPages);
 
-	if (!annots) 
+	for (var p = 0; p < totalPages; p++)
 	{
-		console.println("No annotations found.");
-		return;
-	}
+		console.println("=== Page " + p + " ===");
 
-	console.println("Found " + annots.length + " annotations");
 
-	for (var i = 0; i < annots.length; i++) 
-	{
+		var annots = this.getAnnots({ nPage: p });
 
-		var a = annots[i];
+		if (!annots) 
+		{
+			console.println("No annotations found.");
+			return;
+		}
 
-		console.println("Type: " + a.type);
+		console.println("Found " + annots.length + " annotations");
 
-		if (a.type === "Polygon" || a.type === "PolyLine")
+		for (var i = 0; i < annots.length; i++) 
 		{
 
-			console.println("---- " + a.type + " Found ----");
+			var a = annots[i];
 
-			var verts = a.vertices;
+			console.println("Type: " + a.type);
 
-		
-			if (!verts)
+			if (a.type === "Polygon" || a.type === "PolyLine")
 			{
-				console.println("No vertices!");
-				continue;
-			}
 
-			// get local points from verts
-			var pts = normaliseVertices(verts);
-			
-			// transform to world points
-			var worldPts = affineTransform(pts);
+				console.println("---- " + a.type + " Found ----");
 
-			// print
-			for (var j = 0; j < worldPts.length; j++)
-			{
-				console.println("( " + worldPts[j].x + ", " + worldPts[j].y + " )");
-			}
+				var verts = a.vertices;
 
 			
+				if (!verts)
+				{
+					console.println("No vertices!");
+					continue;
+				}
+
+				// get local points from verts
+				var pts = normaliseVertices(verts);
+				
+				// transform to world points
+				var worldPts = affineTransform(pts);
+
+				// add page id to world points set
+				for (var j = 0; j < worldPts.length; j++)
+				{
+					worldPts[j].page = p;
+				}
+
+				// print
+				for (var j = 0; j < worldPts.length; j++)
+				{
+					console.println("( " + worldPts[j].x + ", " + worldPts[j].y + " ) -- page: " + worldPts[j].page);
+				}
+
+				
+			}
+
 		}
 
 	}
 
 }
+
+
 
 function affineTransform(pts) {
 
